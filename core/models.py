ab78100b86f3c4881f7ed468390717febd2b63cb
druid0523 <druid0523@163.com>
2017-05-04 21:34:06 +0800

    create model

--- core/models.py
+++ core/models.py
@@ -1,5 +1,17 @@
+# -*- coding:utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
 
 # Create your models here.
+
+
+class Device(models.Model):
+    id = models.AutoField(primary_key=True)
+    ctime = models.DateTimeField(auto_now_add=True)
+    mtime = models.DateTimeField(auto_now=True, null=True)
+    sn = models.CharField(max_length=64)
+    imei = models.CharField(max_length=64)
+    os_version = models.CharField(max_length=32)
+    last_heartbeat = models.IntegerField(null=True)
+    stauts = models.CharField(max_length=32)
